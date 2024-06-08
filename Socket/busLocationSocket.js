4c8cb216836e4ca55e5dd30bfa56c0499c3a14d1
Osama-tec <hashmiosama555@gmail.com>
2024-06-09 00:21:31 +0500

    update socket

--- Socket/busLocationSocket.js
+++ Socket/busLocationSocket.js
@@ -1,13 +1,18 @@
 const { Location } = require('../Model/location');
+const { Bus } = require('../Model/Bus');
+const { Route } = require('../Model/route');
 
 const setupBusLocationSocket = (io) => {
-    const BusNamespace = io.of('/bus/location');
-    BusNamespace.on('connection', (socket) => {
+  const BusNamespace = io.of('/bus/location');
+  
+  BusNamespace.on('connection', (socket) => {
     console.log('Client connected');
 
     socket.on('getBusLocation', async (busId) => {
       try {
         const location = await Location.findOne({ busId }).sort({ timestamp: -1 });
+        const bus = await Bus.findById(busId);
+        const route = await Route.findById(bus.routeId).populate('stops');
 
         if (!location) {
           socket.emit('error', 'No location found for the specified bus');
@@ -19,6 +24,14 @@ const setupBusLocationSocket = (io) => {
           latitude: location.latitude,
           longitude: location.longitude,
           timestamp: location.timestamp,
+          route: {
+            origin: route.origin,
+            destination: route.destination,
+            stops: route.stops,
+            distance: route.distance,
+            estimatedTravelTime: route.estimatedTravelTime,
+            waypoints: route.waypoints
+          }
         });
       } catch (error) {
         console.error('Error retrieving bus location:', error);
@@ -32,4 +45,4 @@ const setupBusLocationSocket = (io) => {
   });
 };
 
-module.exports = setupBusLocationSocket ;
+module.exports = setupBusLocationSocket;
